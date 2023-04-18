3101805de3dec3601a5301e0997f84044c8c9420
Alastorial <alex.babykin2003@yandex.ru>
2023-04-18 11:40:57 +0300

    Initial commit

---
+++ src/test/java/ru/alastorial/paidpolyclinic/PaidPolyclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alastorial.paidpolyclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaidPolyclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
